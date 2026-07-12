13d3856061dccbdd6f7a4d619404e08f31577326
raymich <diegoparrales2005@gmail.com>
2026-07-12 18:16:12 -0500

    cambios

--- metro.config.js
+++ metro.config.js
@@ -5,9 +5,23 @@
 // compartan la misma config.
 process.env.EXPO_ROUTER_DISABLE_RN_NAVIGATION_CHECK = '1';
 
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+
+// web/ y backend/ son submódulos (BROKEATE-WEB, BROKEATE-BACKEND) que viven bajo la
+// raíz del proyecto. Metro rastrea la raíz entera, así que sin esto indexaría ambos
+// repos y podría resolver una segunda copia de React desde sus node_modules.
+const escapeRe = (s) => s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+const submodules = ['web', 'backend'].map(
+  (dir) => new RegExp(`^${escapeRe(path.join(__dirname, dir))}[\\\\/].*`)
+);
+
+config.resolver.blockList = [
+  ...[config.resolver.blockList ?? []].flat(),
+  ...submodules,
+];
 
 module.exports = withNativeWind(config, { input: './src/style/global.css' });
